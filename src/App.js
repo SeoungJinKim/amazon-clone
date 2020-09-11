53dccb2be0c179963cdc64849d83ca2aa5b29a0d
SeongJin Kim <dge03078@gmail.com>
2020-09-12 03:09:53 +0900

    Create link function when clicked logo and basket icon
    Click the logo, Go to home page
    Click the basket icon, Go to checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
 	return (
@@ -11,8 +12,16 @@ function App() {
 			<div className="app">
 				{/* Header */}
 				<Header />
-				{/* Home */}
-				<Home />
+				<Switch>
+					<Route path="/checkout">
+						{/* Checkout */}
+						<Checkout />
+					</Route>
+					<Route path="/">
+						{/* Home */}
+						<Home />
+					</Route>
+				</Switch>
 			</div>
 		</Router>
 	);
